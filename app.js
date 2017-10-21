3d2da7bc02f9153df401bf36d8335a57969546db
Trevor Kirpaul <tkirpaul@gmail.com>
2017-10-21 19:30:15 -0400

    renamed public directory to docs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static('docs'));
 
 app.listen(3000, function(){
 	console.log('server running...')
